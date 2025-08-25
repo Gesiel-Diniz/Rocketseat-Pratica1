5de93e2342d075cefc532a419a60a512f2180722
Gesiel Diniz da Silva <43421831+Gesiel-Diniz@users.noreply.github.com>
2025-08-25 20:58:27 -0300

    Exercício 2

---
+++ ExercicioTwo/ExercicioTwo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program()
+{
+    public static void Main()
+    {
+        Console.WriteLine("Digite seu primeiro nome:");
+        string? firstName = Console.ReadLine();
+
+        Console.WriteLine("Digite seu sobrenome:");
+        string? surname = Console.ReadLine();
+
+        if (firstName != null && surname != null)
+        {
+            Console.WriteLine("Seu nome completo é: "+ firstName + " " + surname);
+        }
+        else
+        {
+            Console.WriteLine("Nenhum nome foi informado!");
+        }
+    }
+}
