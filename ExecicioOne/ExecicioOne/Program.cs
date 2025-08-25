27854b3d404264552bd72415d1c56d9d6be0d198
Gesiel Diniz da Silva <43421831+Gesiel-Diniz@users.noreply.github.com>
2025-08-25 20:49:33 -0300

    Exercício 1

---
+++ ExecicioOne/ExecicioOne/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program()
+{
+    public static void Main()
+    {
+        Console.WriteLine("Digite seu nome:");
+        string? nome = Console.ReadLine();
+
+        if(nome != null)
+        {
+            Console.WriteLine("Olá, " + nome + "! Seja muito bem-vindo!");
+        }
+        else
+        {
+            Console.WriteLine("Nenhum nome foi informado!");
+        }
+    }
+}
