6927671423b60ddd5255bbdf03c83fccad136898
Gesiel Diniz da Silva <43421831+Gesiel-Diniz@users.noreply.github.com>
2025-08-25 22:28:57 -0300

    Exercício 4

---
+++ ExercicioFour/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+class Program()
+{
+    public static void Main()
+    {
+
+        int count = 0;
+        string words = "";
+
+        while(words == null || words == "")
+        {
+            Console.WriteLine("Digite uma ou mais palavras:");
+            words = Console.ReadLine();
+        }
+
+        for(int i = 0; i < words.Length; i++)
+        {
+            if(words[i] != ' ')
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine("A palavra(s) tem : " + count + " letras!!");
+
+    }
+
+}
