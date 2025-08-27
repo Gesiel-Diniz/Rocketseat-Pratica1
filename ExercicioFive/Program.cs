24b5eac9d7ff5dad1e27b128f38559c144ce84fd
Gesiel Diniz da Silva <43421831+Gesiel-Diniz@users.noreply.github.com>
2025-08-27 14:04:54 -0300

    Exencício 5

---
+++ ExercicioFive/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program()
+{
+    public static void Main()
+    {
+        Console.WriteLine("Digite uma placa:");
+        string? placa = Console.ReadLine();
+
+        bool size = placa.Length == 7;
+
+        if (!size)
+        {
+            Console.WriteLine("O formato da placa não tem 7 dígitos!!");
+            return;
+        }
+
+        bool isLetters = placa.Substring(0, 3).All(char.IsLetter);
+        bool isNumbers = placa.Substring(3, 4).All(char.IsDigit);
+
+        if (isLetters && isNumbers)
+        {
+            Console.WriteLine("Placa no formato correto!!");
+        }
+        else
+        {
+            Console.WriteLine("Placa no formato incorreto!!");
+        }
+
+
+    }
+}
