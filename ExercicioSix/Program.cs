7b7c9bda97aab3307b38758c693209333376e4bd
Gesiel Diniz da Silva <43421831+Gesiel-Diniz@users.noreply.github.com>
2025-08-27 14:23:05 -0300

    Exercício 6

---
+++ ExercicioSix/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program()
+{
+    public static void Main()
+    {
+
+        DateTime agora = DateTime.Now;
+
+        Console.WriteLine("Data completa: " + agora.ToString("dddd, dd/MM/yyyy"));
+        Console.WriteLine("Data normal: " + agora.ToString("d"));
+        Console.WriteLine("Horas: " + agora.ToString("T"));
+        Console.WriteLine("Horas: " + agora.ToString("dd MMMM yyyy"));
+
+    }
+}
